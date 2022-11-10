b90342917c6615fdf818cc28b0eef892e8c22b3f
Ömer Çağrı Bozdemir <cagribozdemir0@gmail.com>
2022-11-10 14:50:24 +0300

    First commit

---
+++ src/oopWithNLayeredApp/business/ProductManager.java
@@ -0,0 +1,31 @@
+package oopWithNLayeredApp.business;
+
+import java.util.List;
+
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+public class ProductManager {
+	private ProductDao productDao;
+	private Logger[] loggers;
+
+	public ProductManager(ProductDao productDao, Logger[] loggers) {
+		this.productDao = productDao;
+		this.loggers = loggers;
+	}
+
+	public void add(Product product) throws Exception {
+		if (product.getUnitPrice() < 10) {
+			throw new Exception("Ürün fiyatı 10 dan küçük olamaz");
+		}
+
+		productDao.add(product);
+		
+		for (Logger logger : loggers) {
+			logger.log(product.getName());
+		}
+	}
+}
